8935b7a124e8d377dcbce1a3b46572bca22f5fd7
Varadharaja Perumal <varadharaja.perumal@harbingergroup.com>
2024-02-12 11:12:00 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import './App.css';
 import React, { useEffect, useState } from 'react'
 import { Amplify } from "aws-amplify";
 import { get } from "aws-amplify/api";
+import axios from 'axios'
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-const myAPI = "amplify"
+const myAPI = "apiamplify"
 const path = '/customers'; 
 
 const App = () => {
   const [input, setInput] = useState("")
   const [customers, setCustomers] = useState([])
 
+
+  useEffect(()=>{
+    const apiData=async ()=>{
+      try {
+    const result =await axios.get("https://qt163r2o5i.execute-api.us-east-1.amazonaws.com/dev/connecttomysql")
+    console.log("result",result?.data)
+    setInput(result?.data)
+      }
+      catch(e){
+    console.log("eeeeeeeeeeeeeeeeeeeeeee",e)
+      }
+    }
+    apiData()
+      },[])
   //Function to fetch from our backend and update customers array
   function getCustomer(e) {
     let customerId = e.input
@@ -32,15 +47,15 @@ const App = () => {
   async function postTodo() {
     try {
       const restOperation = get({
-        apiName: 'amplify',
-        path: '/customers',
+        apiName: 'apiamplify',
+        path: '/connecttomysql',
         options: {
           body: {
             message: 'Mow the lawn'
           }
         }
       });
-  
+
       const { body } = await restOperation.response;
       const response = await body.json();
   
@@ -54,25 +69,25 @@ const App = () => {
   
 
   return (
-    
-    <div className="App">
-      <h1>Super Simple React App</h1>
-      <div>
-          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
-      </div>
-      <br/>
-      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+    <>{input}</>
+    // <div className="App">
+    //   <h1>Super Simple React App</h1>
+    //   <div>
+    //       <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+    //   </div>
+    //   <br/>
+    //   <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
 
-      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
-      {
-       customers.map((thisCustomer, index) => {
-         return (
-        <div key={thisCustomer.customerId}>
-          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
-        </div>)
-       })
-      }
-    </div>
+    //   <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+    //   {
+    //    customers.map((thisCustomer, index) => {
+    //      return (
+    //     <div key={thisCustomer.customerId}>
+    //       <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+    //     </div>)
+    //    })
+    //   }
+    // </div>
   )
 }
 
